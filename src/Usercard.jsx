e7d37700939a1c08e770a729309cad1bcec5b771
Mohmett <mohmettjama@gmail.com>
2026-02-12 12:59:48 +0300

    Exercise One

---
+++ src/Usercard.jsx
@@ -0,0 +1,11 @@
+
+ const Usercard=()=>{
+    return(
+        <div style={{padding:"10px"}}>
+            <h2>Mohamed</h2>
+            <p>mohamed@gmail.com</p>
+        </div>
+    )
+}
+
+export default Usercard;
