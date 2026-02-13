4c5f4be44bbbb79cd08d3b62fd52724bf3c13096
Mohmett <mohmettjama@gmail.com>
2026-02-13 10:09:10 +0300

    Exercise 3 Assigned

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import  Usercard from "./Usercard";
+import Usercard from "./Usercard";
 import Blog from "./Blog";
-
-function App(){
-  return(
+import UsercardExercise4 from "./UsercardExercise4";
+function App() {
+  return (
     <>
-    <Usercard/>
-    <Blog/>
-
+      <Usercard />
+      <Blog />
+      <UsercardExercise4 name="Ahmed" email="ahmed@info..." />
+      <UsercardExercise4 name="Muqtar" email="muqtar@info..." />
+      <UsercardExercise4 name="Muse" email="muse@info..." />
+      <UsercardExercise4 name="abdi" email="abdi@info..." />
     </>
-
-
-  )
-
+  );
 }
 
 export default App;
---
+++ src/UsercardExercise4.jsx
@@ -0,0 +1,11 @@
+
+ const UsercardExercise4=({name,email})=>{
+    return(
+        <div style={{paddingLeft:"20px"}}>
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    )
+}
+
+export default UsercardExercise4;
